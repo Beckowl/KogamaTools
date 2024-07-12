976bdb902d23e009e2404a6808115ff0c05b8f34
pipocalio <68874587+pipocalio@users.noreply.github.com>
2024-07-12 18:33:29 -0300

    Adicionar arquivos de projeto.

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KogamaTools
+{
+    public class Class1
+    {
+    }
+}
