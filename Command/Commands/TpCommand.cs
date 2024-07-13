51983be5295731f1a47c5f790f42f85adcde718b
pipocalio <68874587+pipocalio@users.noreply.github.com>
2024-07-13 19:41:20 -0300

    Add "/teleport" and  "/tp" commands.

---
+++ Command/Commands/TpCommand.cs
@@ -0,0 +1,33 @@
+﻿namespace KogamaTools.Command.Commands
+{
+    internal class TpCommand : BaseCommand // shorthand for Teleport. I'm not adding name variants to the commands (yet) because that would be too messy
+    {
+        public TpCommand() : base("/tp", "Teleports the camera view to a specified player's position.")
+        {
+           AddVariant(args => Teleport((string)args[0]), typeof(string));
+        }
+
+        private void Teleport(string player)
+        {
+
+            MVPlayer target = AdminToolController.GetPlayer(player);
+
+            if (target == null)
+            {
+                TextCommand.NotifyUser($"<color=yellow>Target player \"{player}\" not found. Check your capitalization and ensure it matches exactly.</color>");
+                return;
+            }
+
+            MVWorldObjectClient WO = MVGameControllerBase.WOCM.GetWorldObjectClient(target.WoId);
+
+            if (WO == null)
+            {
+                TextCommand.NotifyUser("<color=yellow>Target player WOCM not found.</color>");
+                return;
+            }
+
+            MVGameControllerBase.MainCameraManager.CurrentCamera.FocusOnObject(WO);
+            TextCommand.NotifyUser($"<color=cyan>Teleported to {player}.</color>");
+        }
+    }
+}
