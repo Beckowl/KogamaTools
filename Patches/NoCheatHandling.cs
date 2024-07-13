5c7b3c7010a872d5e8e8d733052b4f84d5fb19a2
pipocalio <68874587+pipocalio@users.noreply.github.com>
2024-07-13 18:03:31 -0300

    Enhance anticheat neutralization.

---
+++ Patches/NoCheatHandling.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using HarmonyLib;
+using MV.Common;
+
+namespace KogamaTools.patches
+{
+    internal class NoCheatHandling
+    {
+        [HarmonyPatch(typeof(MVNetworkGame.OperationRequests))]
+        private static class OperationRequestsPatches
+        // most of these aren't needed, but there's no harm in keeping them nice and ineffective, right?
+        {
+            [HarmonyPatch("RevokeEditRights")]
+            [HarmonyPrefix]
+            static bool RevokeEditRights()
+            {
+                return false;
+            }
+
+            [HarmonyPatch("Ban", new Type[] { typeof(int), typeof(MVPlayer), typeof(string) })]
+            [HarmonyPrefix]
+            static bool Ban(ref int hours, ref MVPlayer target, ref string reason)
+            {
+                return false;
+            }
+
+            [HarmonyPatch("Ban", typeof(CheatType))]
+            [HarmonyPrefix]
+            static bool Ban(ref CheatType cheatType)
+            {
+                return false;
+            }
+
+            [HarmonyPatch("Expel")]
+            [HarmonyPrefix]
+            static bool Expel()
+            {
+                return false;
+            }
+
+            [HarmonyPatch("Kick")]
+            [HarmonyPrefix]
+            static bool Kick()
+            {
+                return false;
+            }
+        }
+
+        [HarmonyPatch(typeof(CheatHandling))]
+        private static class CheatHandlingPatches
+        {
+            [HarmonyPatch("Init")]
+            [HarmonyPrefix]
+            static bool Init()
+            {
+                return false;
+            }
+
+            [HarmonyPatch("MachineBanDetected")]
+            [HarmonyPrefix]
+            static bool MachineBanDetected()
+            {
+                return false;
+            }
+
+            [HarmonyPatch("ExecuteBan")]
+            [HarmonyPrefix]
+            static bool ExecuteBan()
+            {
+                return false;
+            }
+        }
+    }
+}
+
