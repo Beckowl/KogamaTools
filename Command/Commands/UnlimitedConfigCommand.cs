4585783499bc69007ec80149a265b67448827925
pipocalio <68874587+pipocalio@users.noreply.github.com>
2024-07-15 19:55:40 -0300

    Add "Unlimited config" feature.

---
+++ Command/Commands/UnlimitedConfigCommand.cs
@@ -0,0 +1,29 @@
+﻿using KogamaTools.Helpers;
+using KogamaTools.Patches;
+
+namespace KogamaTools.Command.Commands
+{
+    internal class UnlimitedConfigCommand : BaseCommand
+    {
+        public UnlimitedConfigCommand() : base("/unlimitedconfig", "")
+        {
+            AddVariant(args => Toggle());
+            AddVariant(args => SetValues((float)args[0], UnlimitedConfig.MaxValue), typeof(float));
+            AddVariant(args => SetValues((float)args[0], (float)args[1]), typeof(float), typeof(float));
+        }
+        private void Toggle()
+        {
+            UnlimitedConfig.Enabled = !UnlimitedConfig.Enabled;
+            NotificationHelper.NotifySuccess($"Unlimited config {(UnlimitedConfig.Enabled ? "Enabled" : "Disabled")}.");
+        }
+        private void SetValues(float minValue, float maxValue)
+        {
+            UnlimitedConfig.MinValue = minValue;
+            UnlimitedConfig.MaxValue = maxValue;
+
+            UnlimitedConfig.Enabled = true;
+
+            NotificationHelper.NotifySuccess($"Unlimited config range set to {UnlimitedConfig.MinValue} - {UnlimitedConfig.MaxValue}.");
+        }
+    }
+}
---
+++ Patches/UnlimitedConfig.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+using HarmonyLib;
+using KogamaTools.Helpers;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace KogamaTools.Patches
+{
+    internal class UnlimitedConfig
+    {
+        public static bool Enabled = ConfigHelper.GetConfigValue<bool>("UnlimitedConfigEnabled");
+        public static float MinValue = ConfigHelper.GetConfigValue<float>("MinValue");
+        public static float MaxValue = ConfigHelper.GetConfigValue<float>("MaxValue");
+
+        [HarmonyPatch(typeof(SettingsSlider))]
+        private static class SettingsSliderPatch
+        {
+            [HarmonyPatch("Initialize", new Type[] { typeof(string), typeof(float), typeof(float), typeof(float) })]
+            [HarmonyPrefix]
+            static void Initialize(ref float value, ref float minValue, ref float maxValue)
+            {
+                if (Enabled)
+                {
+                    minValue = MinValue;
+                    maxValue = MaxValue;
+                    value = Mathf.Clamp(value, MinValue, MaxValue);
+                }
+            }
+
+            [HarmonyPatch("Initialize", new Type[] { typeof(string), typeof(int), typeof(int), typeof(int) })]
+            [HarmonyPrefix]
+            static void Initialize(ref int value, ref int minValue, ref int maxValue)
+            {
+                if (Enabled)
+                {
+                    minValue = (int)MinValue;
+                    maxValue = (int)MaxValue;
+                    value = (int)Mathf.Clamp(value, MinValue, MaxValue);
+                }
+            }
+        }
+
+        // code below is commented out because it causes a crash
+        // I will try to find a better solution later
+        /*
+        [HarmonyPatch(typeof(InputField))]
+        private static class InputFieldPatch
+        {
+            [HarmonyPatch("characterLimit", MethodType.Getter)]
+            [HarmonyPostfix]
+            static void CharacterLimit(ref int __result)
+            {
+                if (Enabled)
+                {
+                    __result = int.MaxValue;
+                }
+                KogamaTools.mls.LogInfo(__result);
+            }
+        }
+        */
+    }
+}
