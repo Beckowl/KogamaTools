c63d5d29684035c1df6e1cfb18cacd3ebf1107d1
pipocalio <68874587+pipocalio@users.noreply.github.com>
2024-07-15 19:54:58 -0300

    Bind configs for future use.

--- Helpers/ConfigHelper.cs
+++ Helpers/ConfigHelper.cs
@@ -18,23 +18,59 @@ namespace KogamaTools.Helpers
 
         public static void BindConfigs()
         {
-            BindConfig("Defaults", "BlueModeEnabled", true, "");
+            // BUILD MODE
 
-            BindConfig("Defaults", "CustomScaleEnabled", false, "");
-            BindConfig("Defaults", "CustomScale", 1.0f, "");
+            BindConfig("BUILD MODE", "BlueModeEnabled", true, "");
 
-            BindConfig("Defaults", "FastLinksEnabled", false, "");
+            BindConfig("BUILD MODE", "CustomScaleEnabled", false, "");
+            BindConfig("BUILD MODE", "CustomScale", 1.0f, "");
 
-            BindConfig("Defaults", "ShowGreetingMessage", true, "");
+            BindConfig("BUILD MODE", "FastLinksEnabled", false, "");
 
-            BindConfig("Defaults", "NoLimitEnabled", false, "");
+            BindConfig("BUILD MODE", "MovementConstraintEnabled", true, "");
 
-            BindConfig("Defaults", "RotationStepEnabled", false, "");
-            BindConfig("Defaults", "RotationStep", 15f, "");
+            BindConfig("BUILD MODE", "NoLimitEnabled", false, "");
 
-            BindConfig("Defaults", "SpeedMultEnabled", false, "");
-            BindConfig("Defaults", "SpeedMult", 1f, "");
-            BindConfig("Defaults", "MovementConstraintEnabled", true, "");
+            BindConfig("BUILD MODE", "RotationStepEnabled", false, "");
+            BindConfig("BUILD MODE", "RotationStep", 15f, "");
+
+            BindConfig("BUILD MODE", "SpeedMultEnabled", false, "");
+            BindConfig("BUILD MODE", "SpeedMult", 1f, "");
+
+            BindConfig("BUILD MODE", "UnlimitedConfigEnabled", false, "");
+            BindConfig("BUILD MODE", "MinValue", 0f, "");
+            BindConfig("BUILD MODE", "MaxValue", 1f, "");
+
+            // PVP
+
+            BindConfig("GRAPHICS", "AntiAFKEnabled", false, "");
+
+            BindConfig("PVP", "CustomFOVEnabled", false, "");
+            BindConfig("PVP", "FOV", 80f, "");
+
+            // GRAPHICS
+
+            BindConfig("GRAPHICS", "FogEnabled", true, "");
+            BindConfig("GRAPHICS", "FogType", 0, "");
+            BindConfig("GRAPHICS", "CustomFogColor", false, "");
+            BindConfig("GRAPHICS", "FogR", 1f, "");
+            BindConfig("GRAPHICS", "FogG", 1f, "");
+            BindConfig("GRAPHICS", "FogB", 1f, "");
+
+            BindConfig("GRAPHICS", "OrtographicCamera", false, "");
+            BindConfig("GRAPHICS", "OrtographicCameraSize", 10f, "");
+
+            BindConfig("GRAPHICS", "RenderDistance", 600f, "");
+
+            BindConfig("GRAPHICS", "ShadowDistance", 10f, "");
+
+            BindConfig("GRAPHICS", "ThemesEnabled", false, "");
+            BindConfig("GRAPHICS", "ForceThemes", false, "");
+            BindConfig("GRAPHICS", "ThemeID", 0, "");
+
+            // MISC
+
+            BindConfig("MISC", "ShowGreetingMessage", true, "");
         }
 
         public static ConfigEntry<T> GetConfig<T>(string key)
