6f0ee6f606e67b5b6550405fcf347f168b857c4f
pipocalio <68874587+pipocalio@users.noreply.github.com>
2024-07-31 16:47:26 -0300

    Change references to those in BepInEx "interop" folder.

---
+++ Patches/FloatErrorFix.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+
+namespace KogamaTools.Patches
+{
+    [HarmonyPatch(typeof(MV.WorldObject.MVMath))]
+    internal class FloatErrorFix
+    {
+        [HarmonyPatch("TryValidateFloat")]
+        [HarmonyPrefix]
+        private static bool TryValidateFloat(ref float f, ref float __result)
+        {
+            KogamaTools.mls.LogInfo(f);
+            if (f == float.NaN)
+            {
+                __result = 0;
+                return false;
+            }
+            return true;
+        }
+    }
+}
