4ed6cbc29a9d34870e1809822a22385d55ab92eb
pipocalio <68874587+pipocalio@users.noreply.github.com>
2024-08-03 18:50:07 -0300

    Add custom crosshair color feature.

---
+++ Command/Commands/CrossHairColorCommand.cs
@@ -0,0 +1,35 @@
+﻿using KogamaTools.Helpers;
+using KogamaTools.Patches;
+using UnityEngine;
+
+namespace KogamaTools.Command.Commands
+{
+    internal class CrossHairColorCommand : BaseCommand
+    {
+        public CrossHairColorCommand() : base("/crosshaircolor", "")
+        {
+            AddVariant(args => SetColor((string)args[0]), typeof(string));
+            AddVariant(args => Toggle());
+        }
+
+        private void Toggle()
+        {
+            // lengthy ass line of code
+            // bad field naming
+            CustomCrossHairColor.CustomColorEnabled = !CustomCrossHairColor.CustomColorEnabled;
+            NotificationHelper.NotifySuccess($"Crosshair color {(CustomCrossHairColor.CustomColorEnabled ? "enabled" : "disabled")}.");
+        }
+
+        private void SetColor(string colorStr) 
+        {
+            if (ColorUtility.TryParseHtmlString(colorStr, out Color color))
+            {
+                CustomCrossHairColor.Color = color;
+                CustomCrossHairColor.CustomColorEnabled = true;
+                NotificationHelper.NotifySuccess($"Crosshair color set to \"{colorStr}\".");
+                return;
+            }
+            NotificationHelper.WarnUser($"The color \"{colorStr}\" is invalid!");
+        }
+    }
+}
---
+++ Patches/CustomCrossHairColor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using HarmonyLib;
+using UnityEngine;
+
+namespace KogamaTools.Patches
+{
+    [HarmonyPatch(typeof(CrossHair))]
+    internal static class CustomCrossHairColor
+    {
+        internal static bool CustomColorEnabled = false;
+        internal static Color Color = Color.green;
+
+        [HarmonyPatch("UpdateCrossHair")]
+        [HarmonyPostfix]
+        private static void UpdateCrossHair(CrossHair __instance)
+        {
+            if (CustomColorEnabled)
+            {
+                if (__instance.crossHair != null)
+                {
+                    __instance.crossHair.color = Color;
+                }
+            }     
+        }
+    }
+}
