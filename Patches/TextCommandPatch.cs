aa076f975687ecd93aef362a0c8a0e2b12ba0fab
pipocalio <68874587+pipocalio@users.noreply.github.com>
2024-08-26 23:18:29 -0300

    Update TextCommandPatch.cs.

--- Patches/TextCommandPatch.cs
+++ Patches/TextCommandPatch.cs
@@ -11,21 +11,12 @@ namespace KogamaTools.Patches
         private static bool Resolve(ref string commandLine)
         {
             TextCommand.Command command = commandLine;
-            string text = command.Name.ToLower();
-            if (text != null)
+
+            if (CommandHandler.TryExecuteCommand(commandLine))
             {
-                if (text == "/abctest" || text == "/assetbundlecachetest")
-                {
-                    TextCommand.Command_AssetBundleCacheTest(command);
-                    return false;
-                }
-                if (CommandHandler.TryExecuteCommand(commandLine))
-                {
-                    return false;
-                }
+                return false;
             }
-            TextCommand.Command_Invalid(command);
-            return false;
+            return true;
         }
     }
 }
