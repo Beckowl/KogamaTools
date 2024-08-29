1d5dc5b66220c3145a12c0426c03f857f1ac9f8f
pipocalio <68874587+pipocalio@users.noreply.github.com>
2024-08-29 00:10:50 -0300

    Add WOReciever.cs.

---
+++ Patches/WOReciever.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+
+namespace KogamaTools;
+
+[HarmonyPatch(typeof(MVWorldObjectClientManagerNetwork))]
+internal static class WOReciever
+{
+    // a substitute for world.InitializedGameQueryData
+    // can't use it because of il2cpp weirdness
+
+    internal static OnWORecievedDelegate OnWORecieved = delegate { };
+    internal delegate void OnWORecievedDelegate(MVWorldObjectClient wo);
+
+    [HarmonyPatch("AddToWorldObjects")]
+    [HarmonyPostfix]
+    private static void AddToWorldObjects(MVWorldObjectClient wo)
+    {
+        if (MVGameControllerBase.IsInitialized)
+        {
+            OnWORecieved.Invoke(wo);
+        }
+    }
+}
