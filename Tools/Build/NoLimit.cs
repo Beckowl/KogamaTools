485c48d61bbe8231dae7d94e50f09e438eb23857
pipocalio <68874587+pipocalio@users.noreply.github.com>
2024-09-03 22:27:55 -0300

    Update No limit to hide modeling constraint.

--- Tools/Build/NoLimit.cs
+++ Tools/Build/NoLimit.cs
@@ -8,6 +8,12 @@ internal static class NoLimit
 {
     internal static bool Enabled = ConfigHelper.GetConfigValue<bool>("NoLimitEnabled");
 
+    [HarmonyPatch(typeof(ConstraintVisualizer), "Init")]
+    [HarmonyPrefix]
+    private static bool Init()
+    {
+        return !Enabled;
+    }
 
     [HarmonyPatch(typeof(ModelingDynamicBoxConstraint), "CanAddCubeAt")]
     [HarmonyPostfix]
