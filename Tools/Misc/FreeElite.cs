1f56dae65e098c02522ab239071993d83bc1b57b
pipocalio <68874587+pipocalio@users.noreply.github.com>
2024-10-02 23:04:20 -0300

    Add FreeElite.cs.

---
+++ Tools/Misc/FreeElite.cs
@@ -0,0 +1,12 @@
+﻿using HarmonyLib;
+
+namespace KogamaTools.Tools.Build;
+internal class FreeElite
+{
+    [HarmonyPatch(typeof(MVClientSettings), nameof(MVClientSettings.IsSubscriber), MethodType.Getter)]
+    [HarmonyPostfix]
+    private static void elite(ref bool __result)
+    {
+        __result = true;
+    }
+}
