82ad7491f302c3f0108e78cdd5aaeb6dcd1000fc
pipocalio <68874587+pipocalio@users.noreply.github.com>
2024-10-29 21:35:19 -0300

    Default fog bool in graphics menu to the world's fog setting.

--- Tools/Graphics/FogModifier.cs
+++ Tools/Graphics/FogModifier.cs
@@ -4,9 +4,23 @@ using UnityEngine;
 namespace KogamaTools.Tools.Graphics;
 
 [HarmonyPatch]
-internal static class FogModifier
+internal class FogModifier : MonoBehaviour
 {
-    internal static bool FogEnabled = false;
+    internal static bool FogEnabled = HasFog();
+
+    private static bool HasFog()
+    {
+        ThemeSkybox[] skyboxes = FindObjectsOfType<ThemeSkybox>();
+        var skyBox = skyboxes.FirstOrDefault();
+
+        if (skyBox != null)
+        {
+            return skyBox.FogEnabled;
+        }
+
+        return true;
+    }
+
     [HarmonyPatch(typeof(RenderSettings), nameof(RenderSettings.fog), MethodType.Setter)]
     [HarmonyPrefix]
     private static void FogPrefix(ref bool value)
