f872c49384db090ce7ccf8715079bb7566e4a7b0
pipocalio <68874587+pipocalio@users.noreply.github.com>
2024-12-01 00:54:34 -0300

    Remove unecessary logic in SingleSidePainting.cs

---
+++ Command/Commands/FindObjectCommand.cs
@@ -0,0 +1,20 @@
+﻿using KogamaTools.Helpers;
+using UnityEngine;
+
+namespace KogamaTools.Command.Commands;
+
+#if DEBUG
+[CommandName("/findobj")]
+[CommandDescription("Finds a gameobject by its name and notifies whether it was found or not.")] // how would you describe it?
+
+internal class FindObjectCommand : BaseCommand
+{
+    [CommandVariant]
+    private void FindObject(string name)
+    {
+        UnityEngine.Object obj = GameObject.Find(name);
+
+        NotificationHelper.NotifyUser((obj == null).ToString());
+    }
+}
+#endif
--- Tools/Build/SingleSidePainting.cs
+++ Tools/Build/SingleSidePainting.cs
@@ -11,29 +11,25 @@ internal static class SingleSidePainting
     [HarmonyPrefix]
     static bool ReplaceCube(PaintCubes __instance, ref CubeModelingStateMachine e)
     {
-        if (!Enabled)
-        {
-            return true;
-        }
+        if (!Enabled) return true;
 
         if (__instance.waitForMouseUp)
         {
             __instance.waitForMouseUp = MVInputWrapper.GetBooleanControl(KogamaControls.PointerSelect);
             return false;
         }
-        bool flag = false;
+
+        bool isPainting = false;
+
         if (MVInputWrapper.GetBooleanControl(KogamaControls.PointerSelect) && e.SelectedCube != null)
         {
-            CubePickingInfo pickingInfo = new CubePickingInfo();
-            if (EditModeObjectPicker.GetPickingInfo(e.TargetCubeModel, ref pickingInfo))
-            {
-                e.HandleAudio(e.SelectedCube.iLocalPos, AudioActions.CubeAdded);
-                e.TargetCubeModel.SetMaterial(e.SelectedCube.iLocalPos, pickingInfo.pickedFace, e.CurrentMaterialId);
-                CubeModelTool.SendCubeEvent(e.TargetCubeModel.CubeCount, EditCubeChange.CubePainted);
-            }
-            flag = true;
+            e.HandleAudio(e.SelectedCube.iLocalPos, AudioActions.CubeAdded);
+            e.TargetCubeModel.SetMaterial(e.SelectedCube.iLocalPos, e.SelectedCube.pickedFace, e.CurrentMaterialId);
+            CubeModelTool.SendCubeEvent(e.TargetCubeModel.CubeCount, EditCubeChange.CubePainted);
+
+            isPainting = true;
         }
-        __instance.paintCursor.UpdateCursor(e.SelectedCube, e.TargetCubeModel, flag);
+        __instance.paintCursor.UpdateCursor(e.SelectedCube, e.TargetCubeModel, isPainting);
         return false;
     }
 }
