983556ab4736303b23b5f0ea7ec270d30e7801f0
pipocalio <68874587+pipocalio@users.noreply.github.com>
2024-12-15 19:42:29 -0300

    Do code cleaning

--- Helpers/RuntimeReferences.cs
+++ Helpers/RuntimeReferences.cs
@@ -1,27 +1,26 @@
-﻿namespace KogamaTools.Helpers
+﻿namespace KogamaTools.Helpers;
+
+internal static class RuntimeReferences
 {
-    internal static class RuntimeReferences
+    internal static DesktopPlayModeController DesktopPlayModeController { get; set; } = null!;
+    internal static DesktopEditModeController DesktopEditModeController { get; private set; } = null!;
+    internal static EditorStateMachine EditorStateMachine { get; private set; } = null!;
+    internal static CubeModelingStateMachine CubeModelingStateMachine { get; private set; } = null!;
+    internal static EditorWorldObjectCreation EditorWorldObjectCreation { get; private set; } = null!;
+
+    internal static void LoadReferences()
     {
-        internal static DesktopPlayModeController DesktopPlayModeController { get; set; } = null!;
-        internal static DesktopEditModeController DesktopEditModeController { get; private set; } = null!;
-        internal static EditorStateMachine EditorStateMachine { get; private set; } = null!;
-        internal static CubeModelingStateMachine CubeModelingStateMachine { get; private set; } = null!;
-        internal static EditorWorldObjectCreation EditorWorldObjectCreation { get; private set; } = null!;
-
-        internal static void LoadReferences()
+        if (MVGameControllerBase.GameMode == MV.Common.MVGameMode.Edit)
         {
-            if (MVGameControllerBase.GameMode == MV.Common.MVGameMode.Edit)
-            {
-                DesktopEditModeController = MVGameControllerBase.EditModeUI.Cast<DesktopEditModeController>();
+            DesktopEditModeController = MVGameControllerBase.EditModeUI.Cast<DesktopEditModeController>();
 
-                EditorStateMachine = DesktopEditModeController.EditModeStateMachine;
-                CubeModelingStateMachine = EditorStateMachine.cubeModelingStateMachine;
-                EditorWorldObjectCreation = DesktopEditModeController.editorWorldObjectCreation;
-            }
-            else if (MVGameControllerBase.GameMode == MV.Common.MVGameMode.Play)
-            {
-                DesktopPlayModeController = MVGameControllerBase.PlayModeUI.Cast<DesktopPlayModeController>();
-            }
+            EditorStateMachine = DesktopEditModeController.EditModeStateMachine;
+            CubeModelingStateMachine = EditorStateMachine.cubeModelingStateMachine;
+            EditorWorldObjectCreation = DesktopEditModeController.editorWorldObjectCreation;
+        }
+        else if (MVGameControllerBase.GameMode == MV.Common.MVGameMode.Play)
+        {
+            DesktopPlayModeController = MVGameControllerBase.PlayModeUI.Cast<DesktopPlayModeController>();
         }
     }
 }
--- Tools/PVP/FastRespawn.cs
+++ Tools/PVP/FastRespawn.cs
@@ -1,58 +1,57 @@
 ﻿using HarmonyLib;
 using MV.Common;
 
-namespace KogamaTools.Tools.PVP
+namespace KogamaTools.Tools.PVP;
+
+[HarmonyPatch]
+internal class FastRespawn
 {
-    [HarmonyPatch]
-    internal class FastRespawn
+    internal static bool Enabled = false;
+    private static void RespawnPlayer()
     {
-        internal static bool Enabled = false;
-        private static void RespawnPlayer()
-        {
-            MVGameControllerBase.GameEventManager.AvatarCommandsPlayMode.SetToSpawnPoint();
-            MVGameControllerBase.GameEventManager.AvatarCommandsPlayMode.EnterPlayingState();
-        }
+        MVGameControllerBase.GameEventManager.AvatarCommandsPlayMode.SetToSpawnPoint();
+        MVGameControllerBase.GameEventManager.AvatarCommandsPlayMode.EnterPlayingState();
+    }
 
-        [HarmonyPatch(typeof(MVAvatarLocal), "Suicide")]
-        [HarmonyPrefix]
-        private static bool Suicide(MVAvatarLocal __instance)
-        {
-            if (!Enabled) return true;
+    [HarmonyPatch(typeof(MVAvatarLocal), "Suicide")]
+    [HarmonyPrefix]
+    private static bool Suicide(MVAvatarLocal __instance)
+    {
+        if (!Enabled) return true;
 
-            if (!__instance.IsInMode(SpawnRoleModeType.Dead))
+        if (!__instance.IsInMode(SpawnRoleModeType.Dead))
+        {
+            if (__instance.interactableLocal.LastDamageSource == null || __instance.interactableLocal.LastDamageSource.Outdated)
             {
-                if (__instance.interactableLocal.LastDamageSource == null || __instance.interactableLocal.LastDamageSource.Outdated)
-                {
-                    __instance.Die();
-                    RespawnPlayer();
-                }
-                else
-                {
-                    __instance.interactableLocal.DieFromRespawn(__instance.interactableLocal.LastDamageSource.shooter, __instance.interactableLocal.LastDamageSource.damageType);
-                }
-            }
-
-            return false;
-        }
-
-        [HarmonyPatch(typeof(AvatarInteractable), "DoKilledNotification")]
-        [HarmonyPatch(typeof(AvatarInteractable), "DieFromFalling")]
-        [HarmonyPatch(typeof(AvatarInteractable), "DieFromStuck")]
-        [HarmonyPatch(typeof(AvatarInteractable), "DieFromBeingStuck")]
-        [HarmonyPrefix]
-        private static void DoKilledNotification(AvatarInteractable __instance)
-        {
-            if (Enabled)
-            {
+                __instance.Die();
                 RespawnPlayer();
             }
+            else
+            {
+                __instance.interactableLocal.DieFromRespawn(__instance.interactableLocal.LastDamageSource.shooter, __instance.interactableLocal.LastDamageSource.damageType);
+            }
         }
 
-        [HarmonyPatch(typeof(DeathUIController), "StartDeathBriefing")]
-        [HarmonyPrefix]
-        private static bool OnSetToDeadMode()
+        return false;
+    }
+
+    [HarmonyPatch(typeof(AvatarInteractable), "DoKilledNotification")]
+    [HarmonyPatch(typeof(AvatarInteractable), "DieFromFalling")]
+    [HarmonyPatch(typeof(AvatarInteractable), "DieFromStuck")]
+    [HarmonyPatch(typeof(AvatarInteractable), "DieFromBeingStuck")]
+    [HarmonyPrefix]
+    private static void DoKilledNotification(AvatarInteractable __instance)
+    {
+        if (Enabled)
         {
-            return !Enabled;
+            RespawnPlayer();
         }
+    }
+
+    [HarmonyPatch(typeof(DeathUIController), "StartDeathBriefing")]
+    [HarmonyPrefix]
+    private static bool OnSetToDeadMode()
+    {
+        return !Enabled;
     }
 }
