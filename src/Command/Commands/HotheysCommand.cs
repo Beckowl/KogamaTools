ff7241c75a1a37b2439b64c1379b26dd86a6a2ba
pipocalio <68874587+pipocalio@users.noreply.github.com>
2024-12-25 11:21:56 -0300

    Add hotkeys command.

---
+++ src/Command/Commands/HotheysCommand.cs
@@ -0,0 +1,16 @@
+﻿using KogamaTools.Helpers;
+
+namespace KogamaTools.Command.Commands;
+
+// i wanted to add this to the info menu but i'm too lazy to do it right now
+
+[CommandName("/hotkeys")]
+[CommandDescription("Lists the hotkeys/shortcuts available.")]
+internal class HotheysCommand : BaseCommand
+{
+    [CommandVariant]
+    private void ShowHotkeys()
+    {
+        NotificationHelper.NotifyUser("• Press <b>F1</b> to toggle the overlay.\n• Press <b>F2</b> to take a screenshot.\n• Hold <b>Ctrl</b> and <b>left click</b> objects to multi-select.\n• Press <b>Ctrl + G</b> when multi-selecting to group the selected objects.");
+    }
+}
--- src/Tools/Misc/GreetingMessage.cs
+++ src/Tools/Misc/GreetingMessage.cs
@@ -3,7 +3,6 @@ using KogamaTools.Helpers;
 
 namespace KogamaTools.Tools.Misc;
 
-[HarmonyPatch]
 internal static class GreetingMessage
 {
     internal static bool ShowGreetingMessage = true;
@@ -13,7 +12,7 @@ internal static class GreetingMessage
         if (ShowGreetingMessage)
         {
             NotificationHelper.NotifySuccess($"Welcome to {KogamaTools.ModName} v{KogamaTools.ModVersion}.");
-            NotificationHelper.NotifyUser("Press F1 to toggle the overlay.\nType /help to see available chat commands.\n");
+            NotificationHelper.NotifyUser("Press <b>F1</b> to toggle the overlay.\nType <b>/help</b> to view available chat commands.\nType <b>/hotkeys</b> to view the available hotkeys.");
 #if DEBUG
             NotificationHelper.WarnUser("<b>This is a debug build intended for testing purposes only. It may be unstable and potentially insecure.</b>");
 #endif
