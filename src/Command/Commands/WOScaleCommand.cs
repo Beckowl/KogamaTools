3c740bcbe2490e530133c8859b84ceafe678e68f
pipocalio <68874587+pipocalio@users.noreply.github.com>
2024-12-25 11:43:25 -0300

    Add WoScale command.

---
+++ src/Command/Commands/WOScaleCommand.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using KogamaTools.Tools.Build;
+using KogamaTools.Helpers;
+
+namespace KogamaTools.Command.Commands;
+
+[CommandName("/woscale")]
+[BuildModeOnly]
+[CommandDescription("Defines a custom scale for new world objects.")]
+internal class WOScaleCommand : BaseCommand
+{
+    [CommandVariant]
+    private void SetScale(float x, float y, float z)
+    {
+        Vector3 scale = new Vector3(x, y, z);
+
+        CustomWOScale.Enabled = true;
+        CustomWOScale.Scale = scale;
+        CustomWOScale.RequestNewGroupIfNecessary();
+
+        NotificationHelper.NotifySuccess($"World object scale set to {scale.ToString()}.");
+    }
+
+    [CommandVariant]
+    private void Toggle()
+    {
+        CustomWOScale.Enabled = !CustomWOScale.Enabled;
+
+        NotificationHelper.NotifySuccess($"Custom world object scale {(CustomWOScale.Enabled ? "enabled" : "disabled")}.");
+    }
+}
